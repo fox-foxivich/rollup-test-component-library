1d9aaa9b3cfc91ba6009db1c425b3ca3ba35b2fc
@v.balabkin <v.balabkin@crym.ru>
2023-01-27 19:57:47 -0800

    updated Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { FC, ButtonHTMLAttributes } from "react"
 
-const Button = () => {
-  return <button>button</button>
+type ButtonProps = {
+  children: React.ReactNode
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"]
+}
+
+const Button: FC<ButtonProps> = ({ children, type = "button" }) => {
+  return <button type={type}>{children}</button>
 }
 
 export default Button
